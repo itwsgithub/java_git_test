c214e0ad1d7c6c5e01d018663f67552d72358f76
vn <ys@itit.com>
2018-09-28 18:58:39 +0800

    第一次提交

---
+++ git_test/src/com/itheima/git/Demo01.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hellow git");
+        System.out.println("hellow git");
+        System.out.println("hellow git");
+        System.out.println("hellow git");
+    }
+}
